2c24d51d8c0dda33c2341defb44991fc502bb4f7
Julia <yulya_9060@mail.ru>
2018-07-17 15:55:08 +0300

    Устранение недочетов

---
+++ utils/backend-stub.js
@@ -0,0 +1,18 @@
+const { generateData, generateDetails } = require('./generate-data');
+
+const data = generateData();
+const cache = {};
+
+exports.initBackendStub = function(app) {
+  app.get('/api/stations', (req, res) => {
+    return res.json(data);
+  });
+
+  app.get('/api/stations/:id', (req, res) => {
+    const id = req.params.id;
+    const info = data[id];
+    const details = cache[id] || (cache[id] = generateDetails(info));
+    res.json({ ...info, ...details });
+    return {};
+  });
+};
